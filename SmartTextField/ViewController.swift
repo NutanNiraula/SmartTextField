86c2ee5877a66eb125563ab608b74e08921591fe
Nutan Niraula <nutan.smartmobe@gmail.com>
2019-04-13 10:37:54 +0545

    Initial Commit

---
+++ SmartTextField/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartTextField
+//
+//  Created by Nutan Niraula on 4/13/19.
+//  Copyright © 2019 Nutan Niraula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
